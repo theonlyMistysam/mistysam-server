0334282cc828c348881d8fd90de8c9e9ed0d6df8
theonlyMistysam <ogunbamwosam31@email.com>
2026-06-14 23:46:02 +0100

    Fix CORS allow all origins

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: ["https://mistysam-client.vercel.app", "http://localhost:5173"],
-  }),
-);
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Connect to MongoDB
